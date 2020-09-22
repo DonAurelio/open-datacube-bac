dfd360d8f65a12699e65e36ec94daf07bb939e2c
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2020-09-22 18:42:14 -0500

    update manual and create cron script

---
+++ explorer/cubedash.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Reference: https://bencane.com/2015/09/22/preventing-duplicate-cron-job-executions/
+
+echo "Testing if another cron is running"
+
+PIDFILE=/home/datacube/explorer/cubedash.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Process already running"
+    exit 0
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+echo "$(date): Running cubedash-gen"
+
+echo "$(date): Running cubedash-gen" >> /home/datacube/explorer/cubedash.log
+
+/home/datacube/miniconda3/bin/cubedash-gen --init --all 1>> /home/datacube/explorer/cubedash.log 2>> /home/datacube/explorer/cubedash.log
+
+echo "$(date): Finished cubedash-gen" >> /home/datacube/explorer/cubedash.log
+
+
+rm $PIDFILE
